252aa43727639090e7d8aa3969a080f9fc79f2c9
marveal <marveal.rab@gmail.com>
2023-09-15 17:43:31 +0800

    - 添加登录界面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,79 +1,64 @@
 <template>
-  <div>
-    <v-img
-      class="mx-auto my-6"
-      max-width="228"
-      src="https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-slim-text-light.svg"
-    ></v-img>
+  <div class="d-flex align-center justify-center login-page">
+    <v-sheet class="mx-auto w-100">
+      <CloudRepoIcon class="mx-auto w-100" height="512px" />
+    </v-sheet>
 
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-      <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-
-      <v-text-field
-        density="compact"
-        placeholder="Email address"
-        prepend-inner-icon="mdi-email-outline"
-        variant="outlined"
-      ></v-text-field>
-
-      <div
-        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+    <v-sheet class="mx-auto w-100">
+      <LogoIcon class="mx-auto my-6 w-100" height="128px"></LogoIcon>
+      <v-card
+        class="mx-auto pa-12 pb-8"
+        elevation="8"
+        max-width="448"
+        rounded="lg"
       >
-        Password
+        <div class="text-subtitle-1 text-medium-emphasis">账号</div>
 
-        <a
-          class="text-caption text-decoration-none text-blue"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
+        <v-text-field
+          density="compact"
+          prepend-inner-icon="mdi-account"
+          variant="outlined"
+        ></v-text-field>
+
+        <div
+          class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
         >
-          Forgot login password?</a
-        >
-      </div>
+          密码
+        </div>
 
-      <v-text-field
-        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
-        :type="visible ? 'text' : 'password'"
-        density="compact"
-        placeholder="Enter your password"
-        prepend-inner-icon="mdi-lock-outline"
-        variant="outlined"
-        @click:append-inner="visible = !visible"
-      ></v-text-field>
+        <v-text-field
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          density="compact"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+        ></v-text-field>
 
-      <v-card class="mb-12" color="surface-variant" variant="tonal">
-        <v-card-text class="text-medium-emphasis text-caption">
-          Warning: After 3 consecutive failed login attempts, you account will
-          be temporarily locked for three hours. If you must login now, you can
-          also click "Forgot login password?" below to reset the login password.
-        </v-card-text>
+        <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+          登录
+        </v-btn>
       </v-card>
-
-      <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
-        Log In
-      </v-btn>
-
-      <v-card-text class="text-center">
-        <a
-          class="text-blue text-decoration-none"
-          href="#"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          Sign up now
-          <v-icon icon="mdi-chevron-right"></v-icon>
-        </a>
-      </v-card-text>
-    </v-card>
+    </v-sheet>
   </div>
   <router-view />
 </template>
 
 <script setup>
-const visible = false;
+import LogoIcon from "./icons/IconLogo.vue";
+import CloudRepoIcon from "./icons/IconCloudRepo.vue";
+import { ref } from "vue";
+
+const visible = ref(false);
 </script>
+
+<style scoped>
+.login-page {
+  height: 100vh;
+  background-color: #f5f5f5;
+}
+
+.login-page .v-sheet {
+  background-color: #f5f5f5;
+}
+</style>
